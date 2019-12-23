0e02c9465d71311568c6550d8abb5f02cef64fb8
Lowry Nakamura <lowry.nakamura@gmail.com>
2019-12-23 15:05:37 -0800

    Updates

--- precache-manifest.0a9a528b0273588fc4428c8037113990.js
+++ precache-manifest.0a9a528b0273588fc4428c8037113990.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8429e63064f23add3fa6b38d0f8e3a41",
+    "revision": "31fc43a1864ad231077a1d4c100611f7",
     "url": "/WovenHeartsHerbFarm/index.html"
   },
   {
-    "revision": "3b1490f73bc3917ed479",
-    "url": "/WovenHeartsHerbFarm/static/css/main.41232bc0.chunk.css"
+    "revision": "47831dba2e8ab8346344",
+    "url": "/WovenHeartsHerbFarm/static/css/main.31e113b7.chunk.css"
   },
   {
-    "revision": "645b94225aa4ad8dd430",
-    "url": "/WovenHeartsHerbFarm/static/js/2.6050a271.chunk.js"
+    "revision": "86669ca3175e3884314e",
+    "url": "/WovenHeartsHerbFarm/static/js/2.ecd22e25.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "/WovenHeartsHerbFarm/static/js/2.6050a271.chunk.js.LICENSE"
+    "url": "/WovenHeartsHerbFarm/static/js/2.ecd22e25.chunk.js.LICENSE"
   },
   {
-    "revision": "3b1490f73bc3917ed479",
-    "url": "/WovenHeartsHerbFarm/static/js/main.3a88eca7.chunk.js"
+    "revision": "47831dba2e8ab8346344",
+    "url": "/WovenHeartsHerbFarm/static/js/main.41b6dc7a.chunk.js"
   },
   {
     "revision": "83606c8d40b34e4a1d69",
     "url": "/WovenHeartsHerbFarm/static/js/runtime-main.aaa1dce2.js"
+  },
+  {
+    "revision": "bc2fec385aef119dfa936657fafca854",
+    "url": "/WovenHeartsHerbFarm/static/media/alyssa.bc2fec38.jpg"
   },
   {
     "revision": "3523fbf3809007e6a6b60317243b3fc6",
@@ -34,6 +38,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "6d06ed4c8d1762aa22eee6a26b3d5ba2",
     "url": "/WovenHeartsHerbFarm/static/media/master2.6d06ed4c.png"
+  },
+  {
+    "revision": "07f6bfe7db93b5a0680a2266f3df823f",
+    "url": "/WovenHeartsHerbFarm/static/media/sophia.07f6bfe7.jpg"
   },
   {
     "revision": "156a8b3d25810ada17be92b4e624d501",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WovenHeartsHerbFarm/precache-manifest.43070226ea161392c0da76bb32eb31aa.js"
+  "/WovenHeartsHerbFarm/precache-manifest.0a9a528b0273588fc4428c8037113990.js"
 );
 
 self.addEventListener('message', (event) => {
